02ecbb151e1b37e47797f30a15e2951215e5ed17
Thiago Furlan <tsfurlan@gmail.com>
2018-11-09 14:17:13 -0200

    adicionou usuario a schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,22 @@
 --criacao do bd
-create database bdmeta
+create database bdmeta;
 --criacao tabela
 create table meta(
     id int,
     nome varchar(100) NOT NULL,
     descricao varchar(1000) NOT NULL,
     prioridade int NOT NULL,
+    idusuario char(11) NOT NULL,
     CONSTRAINT metapk PRIMARY KEY (id),
-    CONSTRAINT prioridadeCheck CHECK (prioridade between 1 and 5))
+    CONSTRAINT usuariofk FOREIGN KEY (idusuario) REFERENCES usuario(cpf)
+        ON UPDATE CASCADE
+        ON DELETE NO ACTION,
+    CONSTRAINT prioridadeCheck CHECK (prioridade between 1 and 5)
+);
+
+create table usuario(
+    cpf char(11),
+    nome varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    CONSTRAINT usuariopk PRIMARY KEY (cpf)
+);
